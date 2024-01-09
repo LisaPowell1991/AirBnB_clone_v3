ace9e8d3650c3bf60fceb640ba53c549086107b7
Jukunye <jukunyes@gmail.com>
2024-01-09 20:38:13 +0300

    import places_reviews

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,12 +1,6 @@
 from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.user import User
-from models.place import Place
-from models.review import Review
 
 """import flask views"""
 from api.v1.views.index import *
@@ -16,4 +10,4 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places_amenities import *
 from api.v1.views.places import *
-
+from api.v1.views.places_reviews import *
